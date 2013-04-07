1ffa60646a67b759dd682a7ea2997b7a90adf276
xxblakefailxx <xxblakefailxx@gmail.com>
2013-04-07 14:53:38 -0400

    Changed out the tcp server to use javascript objects, broke out some functionality to functions. Fixed unique name problem, as well as closing session issue.

--- other/tcp_shiz/tcp-simple-chat.js
+++ other/tcp_shiz/tcp-simple-chat.js
@@ -1,37 +1,90 @@
 var net = require('net');
 var server = net.createServer();
-var sockets = []; // Since it's a chat server we want a central place to keep track of all the connections.
 
+function Socket(name, socket) {
+  this.name = name;
+  this.socket = socket;
+}
+
+Socket.prototype = {
+  constructor: Socket,
+  getName:function() {
+    return this.name;
+  },
+  setName:function(newName) {
+    this.name = newName;
+  },
+  getSocket:function() {
+    return this.socket;
+  },
+  setSocket:function(newSocket) {
+    this.socket = newSocket;
+  }
+}
+var sockets = [];
+
+function broadcast(socket, sockets, writeData, excludeSelf) {
+  sockets.forEach(function(otherSocket) {
+    if (excludeSelf) {
+      if (otherSocket.getSocket() !== socket) {
+        otherSocket.getSocket().write(writeData);
+      }
+    }
+    else {
+      otherSocket.getSocket().write(writeData);
+    }
+  });
+}
+
+function socketIndex(sockets, searchItem) {
+  for (var i = 0; i < sockets.length; i++) {
+    console.log(typeof(searchItem));
+    if (typeof(searchItem) === "string") {
+      if (sockets[i].getName() === searchItem) {
+        return i;
+      }
+    }
+    else {
+      if (sockets[i].getSocket() === searchItem) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
 
 server.on('connection', function(socket) {
   console.log('got a new connection');
-  var identifier = 'user' + sockets.length.toString();
-  sockets.push([identifier, socket]);
-  sockets.forEach(function(otherSocket) {
-    otherSocket[1].write(identifier + " has connected!\n");
-  });
+  var date = new Date();
+  var components = [
+    date.getYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds()
+  ];
+  var id = components.join("");
+  var identifier = 'user' + id;
+
+  sockets.push(new Socket(identifier, socket));
+  broadcast(socket, sockets, identifier + " has connected!\n", false);
 
   socket.on('data', function(data) {
     console.log(identifier + ': ' +  data.toString());
-
-    sockets.forEach(function(otherSocket) {
-      if (otherSocket[1] !== socket) {
-        otherSocket[1].write(identifier.toString() + ": " +  data.toString());
-      }
-    });
+    broadcast(socket, sockets, identifier.toString() + ": " +  data.toString(), true);
   });
   
   socket.on('close', function() {
     console.log('connection closed');
-    var index = sockets.indexOf(socket);
+    var index = socketIndex(sockets, socket);
     sockets.splice(index, 1);
-    sockets.forEach(function(otherSocket) {
-      otherSocket[1].write(identifier.toString() + " has disconnected!");
-    });
+    broadcast(socket, sockets, identifier.toString() + " has disconnected!\n", false);
   });
 
   socket.on('error', function(err) {
-    throw err;
+    console.log(err.message);
   });
 });
 
